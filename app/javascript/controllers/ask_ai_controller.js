8ca7cf611c5c75c34a6115bed208c56ca6744c62
dushyant ramful <dushyantramful@gmail.com>
2023-05-06 22:14:57 +0400

    Can ask AI about dream on another page

--- app/javascript/controllers/ask_ai_controller.js
+++ app/javascript/controllers/ask_ai_controller.js
@@ -1,7 +1,26 @@
-import { Controller } from "@hotwired/stimulus"
+  import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="ask-ai"
-export default class extends Controller {
-  connect() {
+  // Connects to data-controller="ask-ai"
+  export default class extends Controller {
+    static targets = ['form', 'answer']
+
+    connect() {
+      console.log('stimulus controller connected')
+    }
+
+    send(event) {
+      event.preventDefault()
+      console.log('sending request')
+
+      fetch(this.formTarget.action, {
+        method: "POST",
+        headers: { "Accept": "application/json" },
+        body: new FormData(this.formTarget)
+      })
+        .then(response => response.json())
+        .then((data) => {
+          console.log(data.answer)
+          this.answerTarget.innerHTML = data.answer
+        })
+    }
   }
-}
