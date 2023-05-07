46d733843c2f1669ed01b75dc2bd937349ebf12b
dushyant ramful <dushyantramful@gmail.com>
2023-05-07 09:04:13 +0400

    contact page

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,4 @@
 class Message < ApplicationRecord
+  validates :content, presence: true, length: { minimum: 15, maximum: 350 }
+  validates :email, presence: true, format: { with: /\A.+@.+\z/, message: "invalid email format" }
 end
